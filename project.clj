8b54b3e48874dc0806a15a7177d2bf5aa6dc9ece
Mark Woodhall <mark.woodhall@gmail.com>
2016-05-16 21:03:47 +0100

    Add first validation set and exclude clj-time from compojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.4.0"]
+                 [compojure "1.4.0" :exclusions [clj-time]]
                  [ring/ring-defaults "0.1.5"]
                  [clova "0.26.0"]]
   :plugins [[lein-ring "0.9.7"]]
--- src/clova_blog_post/handler.clj
+++ src/clova_blog_post/handler.clj
@@ -1,7 +1,12 @@
 (ns clova-blog-post.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [clova.core :as clova]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(def to-do-validation (clova/validation-set [:title clova/required? clova/stringy? [clova/shorter? 255]
+                                             :date clova/required? clova/date?
+                                             [:meta :category] clova/stringy? [clova/shorter? 255]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
