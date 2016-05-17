28423888f9e1dbfb8c17bd6e31e4a69fa096c125
Mark Woodhall <mark.woodhall@gmail.com>
2016-05-17 20:49:31 +0100

    Add json response and params along with CORS related middleware.

--- src/clova_blog_post/handler.clj
+++ src/clova_blog_post/handler.clj
@@ -1,16 +1,47 @@
 (ns clova-blog-post.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [compojure.handler :as handler]
             [clova.core :as clova]
+            [ring.util.response :refer [response]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-params]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defn allow-cross-origin
+  "middleware function to allow crosss origin"
+  [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (assoc-in response [:headers "Access-Control-Allow-Origin"]
+                "*"))))
+
+(defn options-200
+  "middleware function to always 200 an OPTIONS request"
+  [handler]
+  (fn [request]
+    (if (= :options (:request-method request))
+      {:headers {"Access-Control-Allow-Origin" "*"
+                 "Access-Control-Allow-Methods" "GET, POST, PUT, OPTIONS"}
+       :body ""
+       :status 204}
+      (handler request))))
 
 (def to-do-validation (clova/validation-set [:title clova/required? clova/stringy? [clova/shorter? 255]
                                              :date clova/required? clova/date?
                                              [:meta :category] clova/stringy? [clova/shorter? 255]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (POST "/to-do" [payload]
+        (println payload)
+        (let [validated (clova/validate to-do-validation payload)]
+          (if (:valid? validated)
+            {:body payload :status 200}
+            {:body (:results validated) :status 400})))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> (handler/api app-routes)
+      (options-200)
+      (allow-cross-origin)
+      (wrap-json-response)
+      (wrap-json-params)))
